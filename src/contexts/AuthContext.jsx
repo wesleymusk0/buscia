5535c1bfb33227b7d298697e58a3588732b5fde5
wesleymusk0 <sacwesley0@gmail.com>
2026-04-06 11:08:49 -0300

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -7,25 +7,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile
 } from 'firebase/auth'
-import { getDatabase, ref, get, set } from 'firebase/database'
-import { initializeApp } from 'firebase/app'
-import toast from 'react-hot-toast'
-
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-}
-
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
-const db = getDatabase(app)
+import { app, auth, db } from '../lib/firebase'
 
 const AuthContext = createContext()
 
