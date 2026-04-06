46c3a43aa4158246e6b683c8d4b52212677d5a1f
MrBarbecue YT <sacwesley0@gmail.com>
2026-04-06 10:30:26 -0300

    BUSCIA 2.0

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     }
   },
   server: {
-    allowedHosts: ['buscia.systematrix.com.br'],
+    allowedHosts: ['systematrix.tail2f659b.ts.net'],
     port: 3000,
     host: '0.0.0.0'
   }
